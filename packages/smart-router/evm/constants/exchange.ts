e6ad1cd89315dc3264605b05ab7435753cee7f8f
Frankie <frankie@gmail.com>
2024-04-02 17:04:20 +0700

    Update contract address

--- packages/smart-router/evm/constants/exchange.ts
+++ packages/smart-router/evm/constants/exchange.ts
@@ -50,7 +50,7 @@ export const SMART_ROUTER_ADDRESSES = {
   [ChainId.SEPOLIA]: '0x678Aa4bF4E210cf2166753e054d5b7c31cc7fa86',
   [ChainId.ARBITRUM_SEPOLIA]: '0x678Aa4bF4E210cf2166753e054d5b7c31cc7fa86',
   [ChainId.BASE_SEPOLIA]: '0x678Aa4bF4E210cf2166753e054d5b7c31cc7fa86',
-  [ChainId.U2U_NEBULAS]: '0xd9c3725bb2e76c6c842b2fc653e1e2ad0f248d39',
+  [ChainId.U2U_NEBULAS]: '0xa747e4b107aeae953da024c8a4b97e065502b96c',
 } as const satisfies Record<ChainId, string>
 
 export const V2_ROUTER_ADDRESS: ChainMap<string> = {
