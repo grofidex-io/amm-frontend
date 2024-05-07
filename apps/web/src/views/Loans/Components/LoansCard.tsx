f6f5deb47ba2bbc7e527526b55b6ff82bf92536c
steven <steven@stevens-MacBook-Pro.local>
2024-05-07 15:10:38 +0700

    [Bug] Fix bug handle borrow amount

--- apps/web/src/views/Loans/Components/LoansCard.tsx
+++ apps/web/src/views/Loans/Components/LoansCard.tsx
@@ -212,7 +212,8 @@ const LoansCard = ({ type, stakeInfo, borrowing, nativeBalance, refreshListLoans
   }
 
   const handleBorrow = async (value?: string) => {
-    await callSmartContract(borrowContract.write.borrow([parseEther(value || Number(borrowValue).toFixed(18)), stakeInfo.id, period?.id]), 'You have successfully borrow.')
+    const _value: any = value || borrowValue
+    await callSmartContract(borrowContract.write.borrow([parseEther(new BigNumber(_value).toFixed(18)), stakeInfo.id, period?.id]), 'You have successfully borrow.')
     if(refreshListLoans){ 
      refreshListLoans()
      if(getVaultLoansBalance) {
