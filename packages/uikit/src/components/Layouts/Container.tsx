e27ca32bb950acb8b289f3b5c2e9a46d3ec65acd
hungnd1 <Hungnd1@1293969>
2024-05-23 17:24:48 +0700

    [Task] Update CSS

--- packages/uikit/src/components/Layouts/Container.tsx
+++ packages/uikit/src/components/Layouts/Container.tsx
@@ -1,7 +1,7 @@
 import { Box, BoxProps } from "../Box";
 
 const Container: React.FC<React.PropsWithChildren<BoxProps>> = ({ children, ...props }) => (
-  <Box px={["16px", "16px", "20px", "20px", "24px"]} mx="auto" maxWidth="1248px" {...props}>
+  <Box px={["16px", "16px", "20px", "20px", "24px"]} mx="auto" maxWidth={["1248px", "1248px", "1248px", "1248px", "1248px", "1248px", "1488px"]} {...props}>
     {children}
   </Box>
 );
