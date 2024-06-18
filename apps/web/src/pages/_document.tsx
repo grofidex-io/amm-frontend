543246ed21ee5dce2bd0d32ec7dc850702725f2c
steven <steven@uniultra.xyz>
2024-06-18 09:36:06 +0700

    [Bug] Update gtag on test

--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -50,7 +50,7 @@ class MyDocument extends Document {
         <body>
           <noscript>
             <iframe
-              src="https://www.googletagmanager.com/ns.html?id=G-BJ6E447TDD"
+              src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTAG}`}
               height="0"
               width="0"
               style={{ display: 'none', visibility: 'hidden' }}
